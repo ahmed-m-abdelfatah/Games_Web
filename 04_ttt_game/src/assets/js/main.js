c6a5628284cec3fac04b23d193683d9423503ed2
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2023-01-06 19:39:16 +0200

    ttt-checkpoint

--- 04_ttt_game/src/assets/js/main.js
+++ 04_ttt_game/src/assets/js/main.js
@@ -1,6 +1,10 @@
-// elements
+/**
+ * @Author: Ahmed Abdelfatah
+ * @Date: 2023-01-06 17:25:53
+ * @Desc: elements
+ */
 const game = document.body.querySelector('.ttt-game');
-const cells = {};
+const grid = {};
 const icons = {
   x: game.querySelector('#icon-x'),
   o: game.querySelector('#icon-o'),
@@ -18,14 +22,14 @@ const scores = {
   },
 };
 
-// logic
-// let paused = true;
-
 /**
  * @Author: Ahmed Abdelfatah
- * @Date: 2023-01-06 15:14:32
- * @Desc: for loop to fill cells object and add click event on each cell
+ * @Date: 2023-01-06 17:25:48
+ * @Desc: logic
  */
+const players = ['x', 'o'];
+
+// for loop to fill grid object and add click event on each cell
 for (let y = 1; y <= 3; y++) {
   for (let x = 1; x <= 3; x++) {
     const el = game.querySelector(`.ttt-row:nth-child(${y}) .ttt-cell:nth-child(${x})`);
@@ -35,10 +39,10 @@ for (let y = 1; y <= 3; y++) {
     // add event on each cell
     el.addEventListener('click', cellClickEvent);
 
-    // fill the cells object
-    cells[y] = cells[y] || {};
+    // fill the grid object
+    grid[y] = grid[y] || {};
 
-    cells[y][x] = {
+    grid[y][x] = {
       x,
       y,
       el,
@@ -48,30 +52,83 @@ for (let y = 1; y <= 3; y++) {
 }
 
 function cellClickEvent(e) {
-  // if (paused) {
-  //   return;
-  // }
-
-  const cell = cells[e.target.dataset.y][e.target.dataset.x];
+  const cell = grid[e.target.dataset.y][e.target.dataset.x];
 
   if (!cell.value) {
-    paused = true;
-
     fillCell(cell, 'o');
 
-    // const { winner, empty } = check();
+    const { winner, empty } = check();
+    console.log('winner', winner);
 
     // !winner && play(empty);
   }
 }
 
 function fillCell(cell, value) {
-  // add value of cell in cells object
+  // add value of cell in grid object
   cell.value = value;
-
-  // remove old cell
-  // cell.el.firstChild && cell.el.removeChild(cell.el.firstChild);
 
   // add new cell to ui
   cell.el.appendChild(icons[value].cloneNode(true));
+}
+
+function check(dryRun) {
+  const empty = [];
+  const complete = {
+    player: null,
+    cells: [],
+  };
+
+  // check win
+  for (const axis of ['y', 'x']) {
+    const diagonal = { o: [], x: [] };
+
+    for (let a = 1; a <= 3; a++) {
+      const y = a;
+      const x = axis === 'y' ? y : 4 - y;
+
+      const cell = grid[y][x];
+
+      cell.value && diagonal[cell.value].push(cell);
+
+      const straight = { o: [], x: [] };
+
+      for (let b = 1; b <= 3; b++) {
+        const y = axis === 'y' ? a : b;
+        const x = axis === 'y' ? b : a;
+
+        const cell = grid[y][x];
+
+        cell.value ? straight[cell.value].push(cell) : empty.push(cell);
+      }
+
+      checkLine(straight, complete);
+    }
+
+    checkLine(diagonal, complete);
+  }
+
+  if (!dryRun) {
+    if (complete.player) {
+      scores[complete.player].score++;
+
+      console.log('reset');
+      //   reset();
+    } else if (!empty.length) {
+      console.log('reset');
+      //   reset();
+    }
+  }
+
+  return { winner: complete.player, empty: [...new Set(empty)] };
+}
+
+function checkLine(line, complete) {
+  for (const player of players) {
+    if (line[player].length === 3) {
+      complete.player = player;
+      complete.cells.push(...line[player]);
+      console.log('complete', complete);
+    }
+  }
 }
